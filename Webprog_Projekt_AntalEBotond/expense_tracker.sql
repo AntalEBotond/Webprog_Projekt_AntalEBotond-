92062b498876dd304a785a519359e9b7f117d76a
AntalEBotond <163686424+AntalEBotond@users.noreply.github.com>
2024-12-16 11:11:18 +0200

    Update expense_tracker.sql

--- Webprog_Projekt_AntalEBotond/expense_tracker.sql
+++ Webprog_Projekt_AntalEBotond/expense_tracker.sql
@@ -1,4 +1,4 @@
-your_database_nameyour_database_nameCREATE DATABASE expense_tracker;
+CREATE DATABASE IF NOT EXISTS expense_tracker;
 
 USE expense_tracker;
 
@@ -7,8 +7,21 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 0, 
+    profile_picture VARCHAR(255) DEFAULT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    type ENUM('income', 'expense') NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE expenses (
     id INT AUTO_INCREMENT PRIMARY KEY,
